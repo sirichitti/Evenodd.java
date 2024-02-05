543ef8b00900b28a0e793db7a3203441ed3a23c3
sirichitti <159011256+sirichitti@users.noreply.github.com>
2024-02-05 15:46:10 +0530

    Create Hellouser.java

---
+++ Hellouser.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Hello_user {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
