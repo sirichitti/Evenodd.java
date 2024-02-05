456f0d02e7395510aa0cf07899f1ef6bcd912761
sirichitti <159011256+sirichitti@users.noreply.github.com>
2024-02-05 10:13:14 +0000

    first commit

---
+++ Evenodd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Evenodd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the value : ");
+        int num;
+        num=input.nextInt();
+        evenorodd(num);
+        input.close();
+    }
+    public static int evenorodd(int a)
+    {
+        if(a%2==0)
+        {
+            System.out.println("the number "+a+" is an even number");
+        }
+        else{
+            System.out.println("the number "+a+" is an odd number");
+        }
+        return 0;
+    }
+}
